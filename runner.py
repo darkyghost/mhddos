49a4c8b034c2f7a5d3d0548e892414a2ebd30076
porthole-ascend-cinnamon <porthole-ascend-cinnamon@protonmail.com>
2022-03-27 11:49:15 +0300

    add VPN mode to banner

--- runner.py
+++ runner.py
@@ -238,12 +238,9 @@ def print_banner(vpn_mode):
     print(f'''
                             {cl.HEADER}!!!{'УВІМКНІТЬ VPN!!!' if vpn_mode else 'ВИМКНІТЬ VPN!!!  (окрім UDP атак)'}{cl.RESET}
 
-- {cl.WARNING}Навантаження{cl.RESET} - `-t XXXX` - кількість потоків, за замовчуванням - CPU * 1000
-    python3 runner.py -t 3000 https://ria.ru tcp://194.54.14.131:22
-    
+- {cl.WARNING}Використовувати VPN замість проксі{cl.RESET} - прапорець `--vpn`
+- {cl.WARNING}Навантаження (кількість потоків){cl.RESET} - параметр `-t 3000`, за замовчуванням - CPU * 1000
 - {cl.WARNING}Інформація про хід атаки{cl.RESET} - прапорець `--debug`
-    python3 runner.py --debug https://ria.ru tcp://194.54.14.131:22
-
 - {cl.WARNING}Повна документація{cl.RESET} - https://github.com/porthole-ascend-cinnamon/mhddos_proxy
     ''')
 
