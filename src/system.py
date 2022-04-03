4dc7b4999e5cf18ae61461018f60d796c1f55512
porthole-ascend-cinnamon <porthole-ascend-cinnamon@protonmail.com>
2022-04-03 18:05:00 +0300

    refactor

--- src/system.py
+++ src/system.py
@@ -1,12 +1,12 @@
 from contextlib import suppress
-from sys import platform
 
 
 def fix_ulimits():
-    if platform != 'linux':
+    try:
+        import resource
+    except ImportError:
         return
 
-    import resource
     soft, hard = resource.getrlimit(resource.RLIMIT_NOFILE)
     if soft < hard:
         with suppress(Exception):
