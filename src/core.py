ecb46f72a3037a629106522d08883d61b996bce4
porthole-ascend-cinnamon <porthole-ascend-cinnamon@protonmail.com>
2022-04-18 17:31:37 +0300

    another proxies source

--- src/core.py
+++ src/core.py
@@ -14,6 +14,7 @@ ROOT_DIR = Path(__file__).parent.parent
 PROXIES_URL = random.choice((
     'https://raw.githubusercontent.com/porthole-ascend-cinnamon/proxy_scraper/main/working_proxies.txt',
     'https://raw.githubusercontent.com/porthole-ascend-cinnamon/proxy_scraper/main/working_proxies2.txt',
+    'https://raw.githubusercontent.com/porthole-ascend-cinnamon/proxy_scraper/main/working_proxies3.txt',
 ))
 IT_ARMY_CONFIG_URL = 'https://gist.githubusercontent.com/ddosukraine2022/f739250dba308a7a2215617b17114be9/raw/mhdos_targets_tcp_v2.txt'
 VERSION_URL = 'https://raw.githubusercontent.com/porthole-ascend-cinnamon/mhddos_proxy/main/version.txt'
