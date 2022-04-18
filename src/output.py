792bfa9dc34516eb2e71d4825252a6691af121b0
0xFFiber <0xffiber@gmail..com>
2022-04-18 14:06:39 +0200

    Test lock free counter (somewhat hacky)

--- src/output.py
+++ src/output.py
@@ -1,3 +1,4 @@
+from itertools import count
 import os
 from threading import Lock
 
@@ -32,6 +33,30 @@ class AtomicCounter:
             old = self.value
             self.value = value
         return old
+
+
+class LockFreeAtomicCounter:
+
+    def __init__(self, initial=0):
+        self._increments = count()
+        self._value = initial
+        self._lock = Lock()
+
+    def __iadd__(self, value):
+        self.increment(value)
+        return self
+
+    def increment(self, value=1):
+        for _ in range(value):
+            next(self._increments)
+
+    def reset(self, value=0):
+        with self._lock:
+            old = self._value
+            next_inc = next(self._increments)
+            self._value = value
+            self._increments = count()
+        return old+next_inc-1
 
 
 def show_statistic(statistics, refresh_rate, table, vpn_mode, proxies_cnt, period, passed):
